2d489a025ef4145b0cbbb4c32730f814457a0fbc
Salih Cebeci <salihcebeci@gmail.com>
2021-08-22 17:23:33 +0300

    square control

--- maze.js
+++ maze.js
@@ -4,6 +4,7 @@ DIRECTION_DOWN = 2;
 DIRECTION_RIGHT = 3;
 
 square_size = 40;
+player_size = 20;
 margin_size = 20;
 num_squares = 19;
 
@@ -11,7 +12,8 @@ num_squares = 19;
 var stepCounter = 0;
 
 var player_pos;
-var player_direction = DIRECTION_DOWN;
+var next_pos;
+var player_direction = DIRECTION_RIGHT;
 squares = [
 [0 , 0, 18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //0. sıra
 [0 , 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1], //1. sıra
@@ -35,14 +37,57 @@ squares = [
 
 // Processing method - Baslangicta bir kez
 function setup() {
-  frameRate(5);
+  frameRate(10);
   createCanvas(800, 800);
   colorMode(HSB, 360, 100, 100);
   noStroke();
   player_pos = findPlayerPos();
   drawGameArea();
-  player_pos = [10, 10];
 }
+
+// Processing method - Her frame de
+function draw() {
+  updateNextPos();
+  if (checkPlayerMove())
+		playerMove();
+  else
+    turnLeft();
+
+  drawGameArea();
+}
+
+function updateNextPos(){
+  next_pos = findNextPosition();
+}
+
+function checkPlayerMove(){
+  if (next_pos[0] < 0 || next_pos[0] >= num_squares || next_pos[1] < 0 || next_pos[1] >= num_squares)
+  	return false;
+  if (squares[next_pos[0]][next_pos[1]] == 1)
+    return false;
+  return true;
+}
+
+function findNextPosition(){
+  var next = player_pos.slice();
+  switch(player_direction){
+    case DIRECTION_UP:
+      next[0] = next[0] - 1;
+      break;
+    case DIRECTION_LEFT:
+      next[1] = next[1] - 1;
+      break;
+    case DIRECTION_DOWN:
+      next[0] = next[0] + 1;
+      break;
+    case DIRECTION_RIGHT:
+      next[1] = next[1] + 1;
+      break;
+  }
+  return next;
+}
+
+
 
 function drawMaze(){
     for(var i = 0; i < num_squares ; i++){
@@ -69,50 +114,41 @@ function drawGameArea(){
 }
 
 function drawPlayer(){
-    fill(0, 100, 100);
-		rect(player_pos[1]*square_size + margin_size + 10, player_pos[0]*square_size + margin_size + 10, 20, 20);
-}
-
-// Processing method - Her frame de
-function draw() {
-    if (Math.random() < 0.2 ){   //her 5 adımda bir yönünü değiştir
-      stepCounter = 0;
-      if (Math.random() < 0.5)
-      	turnLeft();
-      else
-        turnRight();
-    }
-    else
-      playerMove();
-    drawGameArea();
+	fill(0, 100, 100);
+    var x = player_pos[1]*square_size + margin_size + 10;
+    var y = player_pos[0]*square_size + margin_size + 10;
+  
+    switch(player_direction){
+    case DIRECTION_UP:
+      triangle(x,y+player_size, x+player_size, y+player_size, x+player_size/2,y);
+      break;
+    case DIRECTION_LEFT:
+      triangle(x+player_size,y, x+player_size, y+player_size, x,y+player_size/2);
+      break;
+    case DIRECTION_DOWN:
+      triangle(x,y, x+player_size, y, x+player_size/2,y+player_size);
+      break;
+    case DIRECTION_RIGHT:
+      triangle(x,y, x, y+player_size, x+player_size,y+player_size/2);
+      break;
+   }
 }
 
 //sağa hareket ettiğimiz fonksiyon player pos'un ilk indexi
 function playerMove(){
-  switch(player_direction){
-    case DIRECTION_UP:
-      player_pos[0] = player_pos[0] - 1;
-      break;
-    case DIRECTION_LEFT:
-      player_pos[1] = player_pos[1] - 1;
-      break;
-    case DIRECTION_DOWN:
-      player_pos[0] = player_pos[0] + 1;
-      break;
-    case DIRECTION_RIGHT:
-      player_pos[1] = player_pos[1] + 1;
-      break;
-  }
+	player_pos = next_pos.slice();
 }
 
 // Yönünü sağa doğru değiştir
 function turnRight(){
 	player_direction = (player_direction + 3)%4;
+  updateNextPos();
 }
 
 // Yönünü sola doğru değiştir
 function turnLeft(){
 	player_direction = (player_direction + 1)%4;
+  updateNextPos();
 }
 
 
